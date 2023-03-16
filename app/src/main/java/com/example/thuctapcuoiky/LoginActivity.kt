05904ac9357e3a57a7ad6dd442b7df7358fea8ce
mvn-nhi.tran <nhi.tran@monstar-lab.com>
2023-03-16 11:33:50 +0700

    UI Splash, Login, Register and handler

---
+++ app/src/main/java/com/example/thuctapcuoiky/LoginActivity.kt
@@ -0,0 +1,39 @@
+package com.example.thuctapcuoiky
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.core.widget.doAfterTextChanged
+import com.example.thuctapcuoiky.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(LayoutInflater.from(this@LoginActivity))
+        setContentView(binding.root)
+        handleListener()
+    }
+
+    private fun handleListener() {
+        binding.apply {
+            edtEmail.doAfterTextChanged {
+
+            }
+            edtPassword.doAfterTextChanged {
+
+            }
+            tvRegister.setOnClickListener {
+                navigationActivity()
+            }
+        }
+    }
+
+    private fun navigationActivity() {
+        val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/thuctapcuoiky/StartActivity.kt
@@ -0,0 +1,33 @@
+package com.example.thuctapcuoiky
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(3000)
+                    startActivity(Intent(this@StartActivity, LoginActivity::class.java))
+                    finish()
+                } catch (e: Exception) {
+                }
+            }
+        }
+        thread.start()
+    }
+}
